f2dd2a9d909c2cb13c9735d1c29f2b9ed5179439
Zeel <zeeldesai2163@gmail.com>
2023-04-02 16:51:40 +0530

    Vercel trying

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,12 @@ connectDB();
 const app = express();
 
 const corsOptions = {
+  origin: `https://${process.env.REACT_APP_BASE_URL}`,
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use(cookieParser());
